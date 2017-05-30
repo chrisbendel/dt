ef6989f8d9337439fe0d2f63622444feb0dc4f39
Chris Bendel <chrissbendel@gmail.com>
2017-05-30 01:23:14 -0400

    need to figure out why yt is so slow

--- src/components/PlayerContainer.js
+++ src/components/PlayerContainer.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
 import EventEmitter from "react-native-eventemitter";
-import {
-  Container,
-  Footer,
-  FooterTab,
-  Spinner,
-  Button,
-  Icon,
-  Text
-} from "native-base";
+import { Container, Footer, FooterTab, Spinner, Icon, Text } from "native-base";
 import {
   AsyncStorage,
   Animated,
   View,
   DeviceEventEmitter,
   TouchableOpacity,
+  Button,
   Dimensions
 } from "react-native";
 import SlidingUpPanel from "react-native-sliding-up-panel";
@@ -43,16 +36,19 @@ function qsToJson(qs) {
 export default class PlayerContainer extends Component {
   constructor(props) {
     super(props);
-    this.getYoutubeStream("LaLINFjBRPM");
-    // this.getScStream("11384003");
+
     let socket;
 
     this.state = {
       room: null,
       song: null,
-      buffering: false,
       panelOpen: false
     };
+  }
+
+  componentWillMount() {
+    this.getYoutubeStream("4piMRBu-N5U");
+    // this.getScStream("11576688");
   }
 
   _statusChanged(status) {
@@ -60,37 +56,65 @@ export default class PlayerContainer extends Component {
   }
 
   getScStream(id) {
+    var a = performance.now();
+
     let url =
       "https://api.soundcloud.com/tracks/" +
       id +
       "/stream?client_id=F8q33BQPCtQHy1sLdye9DriPDNIECjcs";
-    return fetch(url).then(res => {
-      this.setState({ song: res.url });
+    this.player = new Player(url).prepare(() => {
+      this.setState({ song: url });
+      var b = performance.now();
+      console.log("It took " + (b - a) + " ms.");
     });
+
+    // return fetch(url).then(res => {
+    // var a = performance.now();
+
+    //   console.log(res);
+    // this.player = new Player(res.url).prepare();
+    //   this.setState({ song: res.url });
+
+    // var b = performance.now();
+    // console.log("It took " + (b - a) + " ms.");
+    // });
   }
 
   getYoutubeStream(id) {
+    let headers = {
+      headers: {
+        Accept: "text/html",
+        "Content-Type": "text/html",
+        Referer: "https://www.dubtrack.fm",
+        Origin: "https://www.youtube.com"
+      }
+    };
+    var a = performance.now();
     let url = "https://www.youtube.com/get_video_info?html5=1&video_id=" + id;
-    return fetch(url).then(res => {
-      res.text().then(body => {
-        let info = qsToJson(body);
-        var tmp = info["adaptive_fmts"];
-        if (tmp) {
-          tmp = tmp.split(",");
-          for (i in tmp) {
-            tmp[i] = qsToJson(tmp[i]);
-          }
-          info["adaptive_fmts"] = tmp;
-          for (var link of info["adaptive_fmts"]) {
-            console.log(link);
-            if (link.type.includes("audio/mp4")) {
-              // if (link.type.includes("audio/orvis")) {
-              console.log(link.url);
-              this.setState({ song: link.url });
-            }
+    // return fetch(url).then(res => res.text()).then(body => {
+    //   console.log(body);
+    // });
+    return fetch(url, headers).then(res => res.text()).then(body => {
+      console.log(body);
+      let info = qsToJson(body);
+      var tmp = info["adaptive_fmts"];
+      if (tmp) {
+        tmp = tmp.split(",");
+        for (i in tmp) {
+          tmp[i] = qsToJson(tmp[i]);
+        }
+        info["adaptive_fmts"] = tmp;
+        console.log(tmp);
+        for (var link of info["adaptive_fmts"]) {
+          console.log(link.type);
+          if (link.type.includes("audio/mp4")) {
+            this.player = new Player(link.url).prepare();
+            this.setState({ song: link.url });
+            var b = performance.now();
+            console.log("It took " + (b - a) + " ms.");
           }
         }
-      });
+      }
     });
   }
 
@@ -114,10 +138,9 @@ export default class PlayerContainer extends Component {
     let playing = this.state.playing;
     let song = this.state.song;
     let room = this.state.room;
-    console.log(song);
+
     if (song) {
-      // RNAudioStreamer.setUrl(song);
-      this.player = new Player(song);
+      this.player.play();
       return (
         <View>
           <SlidingUpPanel
@@ -130,43 +153,17 @@ export default class PlayerContainer extends Component {
             handlerDefaultView={
               <Container>
                 <Button
+                  title="Play"
                   onPress={() => {
-                    this.player.play();
-                    // this.player.play(() => {
-                    //   MusicControl.setNowPlaying({
-                    //     title: "Billie Jean",
-                    //     artwork: "https://i.imgur.com/e1cpwdo.png", // URL or RN's image 3require()
-                    //     artist: "Michael Jackson",
-                    //     album: "Thriller",
-                    //     genre: "Post-disco, Rhythm and Blues, Funk, Dance-pop",
-                    //     duration: 294, // (Seconds)
-                    //     description: "", // Android Only
-                    //     color: 0xffffff, // Notification Color - Android Only
-                    //     date: "1983-01-02T00:00:00Z" // Release Date (RFC 3339) - Android Only
-                    //   });
-                    // });
-                    // this.toggleRoomPanel();
-                    // console.log(this.panel);
-                    // this.panel.collapsePanel();
+                    this.player.volume = 100;
                   }}
-                >
-                  <Text> Play </Text>
-                </Button>
-
+                />
                 <Button
+                  title="Pause"
                   onPress={() => {
-                    //we can pause or set volume to 0
-                    //Pausing might take extra code to reset the current time of the room
-                    //Might impact slow loading since youtube takes forever initially anyway.
                     this.player.volume = 0;
-                    this.player.pause();
-                    // this.toggleRoomPanel();
-                    // console.log(this.panel);
-                    // this.panel.collapsePanel();
                   }}
-                >
-                  <Text> Pause </Text>
-                </Button>
+                />
               </Container>
             }
           >
@@ -181,101 +178,6 @@ export default class PlayerContainer extends Component {
     }
   }
 }
-
-//   return (
-//     <View style={styles.playerContainer}>
-//       {song
-//         ? <YouTube
-//             ref="youtubePlayer"
-//             videoId={song.songInfo.fkid}
-//             play={playing}
-//             hidden={false}
-//             playsInline={true}
-//             showinfo={false}
-//             apiKey={"AIzaSyBkJJ0ZoT8XbBDYpZ8sVr1OkVev4C5poWI"}
-//             origin={"https://www.youtube.com"}
-//             onChangeState={e => {
-//               if (e.state == "buffering") {
-//                 this.setState({ buffering: true });
-//               } else if (e.state == "ended") {
-//                 this.setState({ song: null });
-//               } else {
-//                 this.setState({ buffering: false });
-//                 this.refs.youtubePlayer.seekTo(song.startTime);
-//               }
-//               console.log(e);
-//             }}
-//             onReady={e => {
-//               console.log(e);
-//               this.refs.youtubePlayer.seekTo(song.startTime);
-//               this.setState({ isReady: true });
-//             }}
-//             style={styles.player}
-//           />
-//         : null}
-//       <View style={styles.info}>
-//         <Text
-//           style={{ fontSize: 11, fontWeight: "bold" }}
-//           numberOfLines={1}
-//         >
-//           {room.name}
-//         </Text>
-//       </View>
-//       {song
-//         ? <View style={styles.info}>
-//             <Text style={{ fontSize: 11 }} numberOfLines={1}>
-//               {song.songInfo.name}
-//             </Text>
-//           </View>
-//         : null}
-//       <Footer style={{ borderTopWidth: 0 }}>
-//         <FooterTab>
-//           <Button
-//             onPress={() => {
-//               //updub the song
-//             }}
-//           >
-//             <Icon name="ios-arrow-up" />
-//           </Button>
-//           <Button
-//             onPress={() => {
-//               //downdub song
-//             }}
-//           >
-//             <Icon name="ios-arrow-down" />
-//           </Button>
-//           <Button
-//             onPress={() => {
-//               Actions.room({ title: room.name });
-//             }}
-//           >
-//             <Icon name="ios-chatbubbles" />
-//           </Button>
-//           <Button
-//             onPress={() => {
-//               if (playing) {
-//                 this.setState({ playing: false });
-//               } else {
-//                 this.getSongTime(room);
-//               }
-//             }}
-//           >
-//             {this.state.buffering
-//               ? <Spinner color="black" />
-//               : // <Icon name=".ion-loading-c"/>
-//                 <Icon
-//                   name={playing ? "ios-volume-up" : "ios-volume-off"}
-//                 />}
-//           </Button>
-//         </FooterTab>
-//       </Footer>
-//     </View>
-//   );
-// } else {
-//   return null;
-// }
-// }
-// }
 
 const styles = {
   container: {
